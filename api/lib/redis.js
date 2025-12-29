771919926be59f6263786bf2aaaa6ec685ccc194
Abhishek Kumar <abhishekprajapati243@gmail.com>
2025-12-29 16:02:06 +0530

    first commit

--- api/lib/redis.js
+++ api/lib/redis.js
@@ -1,6 +1,6 @@
 import { createClient } from '@vercel/kv';
 
 export const kv = createClient({
-    url: process.env.KV_REST_API_URL,
-    token: process.env.KV_REST_API_TOKEN,
+    url: process.env.KV_REST_API_URL || process.env.UPSTASH_REDIS_REST_URL,
+    token: process.env.KV_REST_API_TOKEN || process.env.UPSTASH_REDIS_REST_TOKEN,
 });
